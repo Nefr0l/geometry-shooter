7a5ddf027a1147a795d0391c52200eb32641806c
Nefr0l <filip.wrzosek07@gmail.com>
2025-06-29 11:53:09 +0200

    feat: add acceleration to player movement

--- player/player_movement.gd
+++ player/player_movement.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
+var acceleration = 1.75
+
 func _physics_process(delta: float) -> void:
 	var directionX := Input.get_axis("ui_left", "ui_right")
 	var directionY := Input.get_axis("ui_up", "ui_down")
 	
+	if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
+		acceleration = 1.75
+	
+	acceleration = max(acceleration - (delta *2), 1)
+	
 	if directionX or directionY:
-		velocity.x = directionX * GameManager.player_speed
-		velocity.y = directionY * GameManager.player_speed
+		velocity.x = directionX * GameManager.player_speed * acceleration
+		velocity.y = directionY * GameManager.player_speed * acceleration
 	else:
 		velocity.x = move_toward(velocity.x, 0, GameManager.player_speed)
 		velocity.y = move_toward(velocity.y, 0, GameManager.player_speed)
